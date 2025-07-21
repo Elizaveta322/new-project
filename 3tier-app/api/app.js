5a90146853b729dc6d1c6f84c98c8d1b3fe93f62
Elizaveta322 <elizaveta.tim@edu.312school.com>
2025-07-21 15:31:21 -0500

    Fix: remove submodule and re-add as normal folder

---
+++ 3tier-app/api/app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var uuid = require('node-uuid');
+
+var pg = require('pg');
+const conString = {
+    user: process.env.DBUSER,
+    database: process.env.DB,
+    password: process.env.DBPASS,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT
+};
+
+// Routes
+app.get('/api/status', function(req, res) {
+  const uuidv4 = require('uuid').v4;
+  res.status(200).json([{
+    request_uuid: uuidv4(),
+    time: new Date().toISOString()
+  }]);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
